222542ee60c6d3ed51c94ccc2f391085438ee9a2
AleCevar <al.cerdas@estudiantec.cr>
2026-01-10 18:00:17 -0600

    Changes by kencast

---
+++ Tarea 4/Pruebas/Grafico_Chi_Cuadrado.py
@@ -0,0 +1,29 @@
+import numpy as np
+from scipy.stats import norm, chi2
+import matplotlib.pyplot as plt
+
+def mostrar(chi_obs, df):
+    alpha = 0.05
+    # aproximación normal
+    mu = df
+    sigma = np.sqrt(2*df)
+
+    z = norm.ppf(1 - alpha)
+    chi_right = mu + z*sigma
+
+    x = np.linspace(mu - 5*sigma, mu + 5*sigma, 2000)
+    y = norm.pdf(x, mu, sigma)
+
+    plt.plot(x, y, label='Aprox. normal a χ²')
+
+    plt.fill_between(x, y, where=(x >= chi_right), alpha=0.3)
+
+    plt.axvline(chi_right, linestyle='--', label='Crítico der')
+    plt.axvline(chi_obs, linewidth=2, label=r'$\chi^2_{obs}$')
+
+    plt.xlabel(r'$\chi^2$')
+    plt.ylabel('Densidad')
+    plt.title(r'$\chi^2$')
+    plt.legend()
+    plt.grid(True)
+    plt.show()
